3dd02928e337605645e1a1b765d3b3c023011857
prinzprogrammer <princeprogrammer73@gmail.com>
2023-11-04 19:35:05 +0800

    Created basic Project

---
+++ src/Project.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Prince
+ * Program Description: Demo Files for COMP1011 Lesson 03
+ * @version 0.1 - Created basic Project
+ */
+public class Project {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
