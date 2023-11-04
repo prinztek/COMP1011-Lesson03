9ffa7575118d8ce2d1b523364a7648dd25b161bb
prinzprogrammer <princeprogrammer73@gmail.com>
2023-11-04 21:51:15 +0800

    Added Vehicle Class

--- src/Project.java
+++ src/Project.java
@@ -5,15 +5,16 @@
 /**
  * @author Prince
  * Program Description: Demo Files for COMP1011 Lesson 03
- * @version 0.1 - Created basic Project
+ * @version 0.2 - Added Vehicle Class
  */
 public class Project {
 
 	/**
+	 * Main Method for project
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Vehicle myVehicle = new Vehicle();
 
 	}
 
